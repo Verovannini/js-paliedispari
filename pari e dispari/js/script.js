48e356a3c21121e7a6f2f07fd93836734393f815
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-03-25 18:27:14 +0100

    fine pari o dispari

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,4 +1,6 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 // Chiedere all'utente pari o dispari
 var pariODispari = prompt('Pari o dispari?');
@@ -12,13 +14,58 @@ console.log(numeroUtente);
 var randomNumberComputer = randomNumber(1, 5);
 console.log(randomNumberComputer);
 
+// Sommare i due numeri
+var sommaNumeri = somma(numeroUtente, randomNumberComputer);
+console.log(sommaNumeri);
+
+// Stabilire se la somma dei numeri è pari o dispari
+var OddEven = numeroPariODispari(sommaNumeri);
+console.log(OddEven);
+
+// Confrontare PariODispari con OddEven e stampare chi ha vinto
+var messaggio;
+if( pariODispari == OddEven ) {
+    messaggio = 'Hai vinto!';
+} else {
+    messaggio = 'Hai perso!';
+}
+alert(messaggio);
+
+
 // Creare funzione per generare un numero random
+// 
+// Funzione che genera un numero random
+// 
+// min -> Inserire un numero che sarà il minimo generabile
+// max -> Inserire un numero che sarà il massimo generabile
+// 
+// return: ritorna un numero compreso tra il minimo e il massimo
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-// Sommare i due numeri
+// Funzione che somma due numeri
+// 
+// num1 e num2 -> inserire due numeri che poi verranno sommati
+// 
+// return: ritorna la somma dei numeri
+function somma(num1, num2) {
+    var risultato = num1 + num2;
+    return risultato;
+}
 
-// Stabilire se la somma dei numeri è pari o dispari
+// Funzione che stabilisce se un numero è pari o dispari
+// 
+// numero -> inserire un numero
+// 
+// return: ritorna 'pari' se la parola è pari e 'dispari' se è dispari
+function numeroPariODispari(numero){
+    var risultatoPariODispari;
 
-// Stampare chi ha vinto
+    if(numero % 2 == 0) {
+        risultatoPariODispari = 'pari';
+    } else {
+        risultatoPariODispari = 'dispari';
+    }
+    return risultatoPariODispari;
+}
